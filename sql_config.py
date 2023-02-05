d06876b230a932e7aaa285677e06d7812624d19a
Fernando Kong <74848434+fernandobeast@users.noreply.github.com>
2023-02-06 07:22:14 +0800

    PostgreSQL and API key config file

--- sql_config.py
+++ sql_config.py
@@ -1,6 +1,6 @@
 USERNAME = 'your_sql_user_name'
 PASSWORD = 'your_sql_password'
-IPADDRESS = 'your_sql_Ip'
+IPADDRESS = 'your_sql_Ip_address'
 PORT = your_sql_port
 DB_NAME = 'your_sql_database_name'
 API_KEY = 'polygon.io_api_key'
